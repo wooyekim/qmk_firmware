d103811d77953f1fcea78e60150fcaa050fc7d74
Wooyeol Kim <wooyekim@gmail.com>
2024-03-24 15:23:57 +0900

    Add Charybdis nano keymap v1

---
+++ keyboards/bastardkb/charybdis/3x5/keymaps/wooyekim/features/send_string.c
@@ -0,0 +1,19 @@
+#include "send_string.h"
+
+bool process_send_string_map(uint16_t keycode, keyrecord_t *record) {
+  uint8_t idx = 0;
+  const send_string_t* entry;
+  while (true) {
+    entry = send_string_map[idx++];
+    if (entry == NULL) {
+      return true;
+    }
+    if (entry->keycode == keycode) {
+      break;
+    }
+  }
+  if (record->event.pressed) {
+    SEND_STRING_DELAY(entry->str, 10 /* ms */);
+  }
+  return false;
+}
---
+++ keyboards/bastardkb/charybdis/3x5/keymaps/wooyekim/rules.mk
@@ -0,0 +1,12 @@
+OS_DETECTION_ENABLE = yes
+COMBO_ENABLE = yes
+CONSOLE_ENABLE = yes
+TRI_LAYER_ENABLE = yes
+
+SRC += features/auto_completion.c
+SRC += features/custom_oneshot_mods.c
+SRC += features/custom_ths_keys.c
+SRC += features/layer_lock.c
+SRC += features/os.c
+SRC += features/send_string.c
+
